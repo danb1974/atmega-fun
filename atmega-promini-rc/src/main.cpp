43fec907d21ae92c7a0901544fe65aedf37da20d
Dan Borlovan <danb1974@users.noreply.github.com>
2024-10-13 18:19:06 +0300

    Refactor (stage 2)

--- atmega-promini-rc/src/main.cpp
+++ atmega-promini-rc/src/main.cpp
@@ -2,6 +2,11 @@
 
 //#define DEBUG
 
+// channel count and indexes (order does not actually matter but keep same as on receiver)
+#define CHN_COUNT 2
+#define CHN_STR 0
+#define CHN_THR 1
+
 // inputs from RC receiver (must be INT - not PCINT - capable pins)
 #define PIN_STR_INPUT 2   // steering channel on INT0
 #define PIN_THR_INPUT 3   // throttle channel on INT1
@@ -12,51 +17,40 @@
 
 //
 
-volatile uint32_t str_up_micros = 0;   // last up event
-volatile uint32_t str_pulse_width = 0; // last pulse width
-volatile uint32_t thr_up_micros = 0;
-volatile uint32_t thr_pulse_width = 0;
+volatile uint32_t chnLastPulseStart[CHN_COUNT];
+volatile uint32_t chnLastPulseWidth[CHN_COUNT];
 
-void str_interrupt() {
-  uint32_t now = micros();
-
-  uint8_t state = digitalRead(PIN_STR_INPUT);
-  if (state == 1) {
-    str_up_micros = now;
+void handleChnEvent(uint32_t now, uint8_t chnIndex, uint8_t chnState) {
+  if (chnState == 1) {
+    // up transition
+    chnLastPulseStart[chnIndex] = now;
     return;
   }
   
-  if (str_up_micros == 0) {
+  if (chnLastPulseStart[chnIndex] == 0) {
+    // no up transition yet
     return;
   }
 
-  // state == 0
-  str_pulse_width = now - str_up_micros;
-  if (str_pulse_width < 1000 || str_pulse_width > 2000) {
+  // down transition
+  uint16_t pulseWidth = now - chnLastPulseStart[chnIndex];
+  if (pulseWidth < 1000 || pulseWidth > 2000) {
     // invalid
-    str_pulse_width = 0;
+    pulseWidth = 0;
   }
+  chnLastPulseWidth[chnIndex] = pulseWidth;
 }
 
-void thr_interrupt() {
+void strInterrupt() {
   uint32_t now = micros();
+  uint8_t state = digitalRead(PIN_STR_INPUT);
+  handleChnEvent(now, CHN_STR, state);
+}
 
+void thrInterrupt() {
+  uint32_t now = micros();
   uint8_t state = digitalRead(PIN_THR_INPUT);
-  if (state == 1) {
-    thr_up_micros = now;
-    return;
-  }
-  
-  if (thr_up_micros == 0) {
-    return;
-  }
-
-  // state == 0
-  thr_pulse_width = now - thr_up_micros;
-  if (thr_pulse_width < 1000 || thr_pulse_width > 2000) {
-    // invalid
-    thr_pulse_width = 0;
-  }
+  handleChnEvent(now, CHN_THR, state);
 }
 
 void setup() {
@@ -70,8 +64,8 @@ void setup() {
   pinMode(PIN_STR_INPUT, INPUT);
   pinMode(PIN_THR_INPUT, INPUT);
 
-  attachInterrupt(digitalPinToInterrupt(PIN_STR_INPUT), str_interrupt, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(PIN_THR_INPUT), thr_interrupt, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PIN_STR_INPUT), strInterrupt, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PIN_THR_INPUT), thrInterrupt, CHANGE);
 
 #ifdef DEBUG
   Serial.println("Interrupts set, ready to roll");
@@ -93,14 +87,16 @@ uint8_t pulseWidthToLedValue(uint16_t pulseWidth) {
 }
 
 void loop() {
-  uint8_t str_led_value = pulseWidthToLedValue(str_pulse_width);
-  uint8_t thr_led_value = pulseWidthToLedValue(thr_pulse_width);
+  uint8_t ledValues[CHN_COUNT];
+  for (uint8_t i = 0; i < CHN_COUNT; i++) {
+    ledValues[i] = pulseWidthToLedValue(chnLastPulseWidth[i]);
+  }
 
-  analogWrite(PIN_STR_LED, str_led_value);
-  analogWrite(PIN_THR_LED, pulseWidthToLedValue(thr_pulse_width));
+  analogWrite(PIN_STR_LED, ledValues[CHN_STR]);
+  analogWrite(PIN_THR_LED, ledValues[CHN_THR]);
 
-  printPulseData("STR", str_pulse_width, str_led_value);
-  printPulseData("THR", thr_pulse_width, thr_led_value);
+  printPulseData("STR", chnLastPulseWidth[CHN_STR], ledValues[CHN_STR]);
+  printPulseData("THR", chnLastPulseWidth[CHN_THR], ledValues[CHN_THR]);
 
 #ifdef DEBUG
   delay(100);
