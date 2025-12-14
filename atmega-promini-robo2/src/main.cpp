b8d957463c14ed1fa4463d4ae031886a4d2d634b
Dan Borlovan <danb1974@gmail.com>
2025-12-14 18:47:18 +0200

    Proximity sensor support; refactor

--- atmega-promini-robo2/src/main.cpp
+++ atmega-promini-robo2/src/main.cpp
@@ -4,8 +4,14 @@
 #include <FastLED.h>
 
 // serial led matrix 4x4
-#define LED_PIN 9
+#define LED_PIN 7
 #define LED_COUNT 16
+
+// proximity corner sensors
+#define PIN_PROX_FR 8
+#define PIN_PROX_FL 9
+#define PIN_PROX_RR 12
+#define PIN_PROX_RL 13
 
 static CRGB ledStrip[LED_COUNT];
 
@@ -53,6 +59,16 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Initializing");
 
+  // proximity sensors
+  pinMode(PIN_PROX_FR, INPUT);
+  pinMode(PIN_PROX_FL, INPUT);
+  pinMode(PIN_PROX_RR, INPUT);
+  pinMode(PIN_PROX_RL, INPUT);
+
+  // rc signal inputs
+  pinMode(PIN_STR, INPUT);
+  pinMode(PIN_THR, INPUT);
+
   // make sure motors are stopped
   pinMode(PIN_MOTOR_1A, OUTPUT);
   pinMode(PIN_MOTOR_1B, OUTPUT);
@@ -62,19 +78,18 @@ void setup() {
   analogWrite(PIN_MOTOR_1B, 0);
   analogWrite(PIN_MOTOR_2A, 0);
   analogWrite(PIN_MOTOR_2B, 0);
+  // and do a short twitch to test them
+  testMotors();
 
+  // led matrix
   FastLED.addLeds<WS2812, LED_PIN, GRB>(ledStrip, LED_COUNT);
+  // and do a short test pattern
   FastLED.setBrightness(3);
   for (int32_t i = 0xff0000; i != 0; i >>= 8) {
     FastLED.showColor(CRGB(i));
     delay(100);
   }
   FastLED.showColor(CRGB(0));
-
-  pinMode(PIN_STR, INPUT);
-  pinMode(PIN_THR, INPUT);
-
-  testMotors();
 
   analogWrite(LED_BUILTIN, 255);
   Serial.println("Running");
@@ -185,6 +200,29 @@ void loop() {
     if (thr2Percent > 100) thr2Percent = 100;
     if (thr2Percent < -100) thr2Percent = -100;
     
+    bool prox_fr = !digitalRead(PIN_PROX_FR); 
+    bool prox_fl = !digitalRead(PIN_PROX_FL);
+    bool prox_rr = !digitalRead(PIN_PROX_RR);
+    bool prox_rl = !digitalRead(PIN_PROX_RL);
+
+    // block motion in direction of proximity sensors
+
+    if (prox_fr || prox_fl) {
+      if (thr1Percent > 0 || thr2Percent > 0) {
+        thr1Percent = 0;
+        thr2Percent = 0;
+      }
+    }
+
+    if (prox_rr || prox_rl) {
+      if (thr1Percent < 0 || thr2Percent < 0) {
+        thr1Percent = 0;
+        thr2Percent = 0;
+      }
+    }
+
+    // steering mix
+
     if (thr1Percent > 0) {
       motor1a = thr1Percent * 2;
     } else if (thr1Percent < 0) {
@@ -197,41 +235,15 @@ void loop() {
       motor2b = -thr2Percent * 2;
     }
 
+    // update motors
+
     analogWrite(PIN_MOTOR_1A, motor1a);
     analogWrite(PIN_MOTOR_1B, motor1b);
     analogWrite(PIN_MOTOR_2A, motor2a);
     analogWrite(PIN_MOTOR_2B, motor2b);
 
-    #if 0
-    // hsv: 0 = red, 96 = green
-    static uint8_t bar1O[4] = {15, 8, 7, 0};
-    static uint8_t bar1I[4] = {14, 9, 6, 1};
-    static uint8_t bar2I[4] = {13, 10, 5, 2};
-    static uint8_t bar2O[4] = {12, 11, 4, 3};
+    // draw motor speed on led matrix
 
-    uint8_t hsv1V = floor(abs(thr1Percent) * 96U / 100U);
-    uint8_t hsv2V = floor(abs(thr2Percent) * 96U / 100U);
-    uint8_t hsv1L = floor(min(abs(thr1Percent), 99U) / 25U);
-    uint8_t hsv2L = floor(min(abs(thr2Percent), 99U) / 25U);
-
-    for (uint8_t i = 0; i < LED_COUNT; i++) {
-      ledStrip[i] = CHSV(0, 0, 0);
-    }
-    if (thr1Percent != 0) {
-      for (uint8_t l = 0; l <= hsv1L; l++) {
-        uint8_t i = (thr1Percent > 0) ? l : 3 - l;
-        ledStrip[bar1I[i]] = CHSV(hsv1V, 255, 255);
-        ledStrip[bar1O[i]] = CHSV(hsv1V, 255, 255);
-      }
-    }
-    if (thr2Percent != 0) {
-      for (uint8_t l = 0; l <= hsv2L; l++) {
-        uint8_t i = (thr2Percent > 0) ? l : 3 - l;
-        ledStrip[bar2I[i]] = CHSV(hsv2V, 255, 255);
-        ledStrip[bar2O[i]] = CHSV(hsv2V, 255, 255);
-      }
-    }
-    #else
     static uint8_t bar1[8] = {14, 15, 8, 9, 6, 7, 0, 1};
     static uint8_t bar2[8] = {13, 12, 11, 10, 5, 4, 3, 2};
 
@@ -243,21 +255,59 @@ void loop() {
     for (uint8_t i = 0; i < LED_COUNT; i++) {
       ledStrip[i] = CHSV(0, 0, 0);
     }
+
     if (thr1Percent != 0) {
       for (uint8_t l = 0; l <= hsv1L; l++) {
         uint8_t i = (thr1Percent > 0) ? l : 7 - l;
         ledStrip[bar1[i]] = CHSV(hsv1V, 255, 255);
       }
     }
+
     if (thr2Percent != 0) {
       for (uint8_t l = 0; l <= hsv2L; l++) {
         uint8_t i = (thr2Percent > 0) ? l : 7 - l;
         ledStrip[bar2[i]] = CHSV(hsv2V, 255, 255);
       }
     }
-    #endif
+
+    // now draw proximity on led matrix
+    // this can be deduplicated by using arrays of sensors and associated leds
+
+    #define PROX_LED_CNT 3
+    #define PROX_LED_HUE 190 // violet-ish
+
+    static uint8_t prox_led_fr[PROX_LED_CNT] = {2, 3, 4};
+    if (prox_fr) {
+      for (uint8_t i = 0; i < PROX_LED_CNT; i++) {
+        ledStrip[prox_led_fr[i]] = CHSV(PROX_LED_HUE, 255, 255);
+      }
+    }
+
+    static uint8_t prox_led_fl[PROX_LED_CNT] = {0, 1, 7};
+    if (prox_fl) {
+      for (uint8_t i = 0; i < PROX_LED_CNT; i++) {
+        ledStrip[prox_led_fl[i]] = CHSV(PROX_LED_HUE, 255, 255);
+      }
+    }
+
+    static uint8_t prox_led_rr[PROX_LED_CNT] = {11, 12, 13};
+    if (prox_rr) {
+      for (uint8_t i = 0; i < PROX_LED_CNT; i++) {
+        ledStrip[prox_led_rr[i]] = CHSV(PROX_LED_HUE, 255, 255);
+      }
+    }
+
+    static uint8_t prox_led_rl[PROX_LED_CNT] = {8, 14, 15};
+    if (prox_rl) {
+      for (uint8_t i = 0; i < PROX_LED_CNT; i++) {
+        ledStrip[prox_led_rl[i]] = CHSV(PROX_LED_HUE, 255, 255);
+      }
+    }
+
     FastLED.show();
     
+    // done
+
     analogWrite(LED_BUILTIN, 255);
 
   } else {
